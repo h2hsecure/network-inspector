e5228022912d257ef2f5f7da74dffd0722f36757
root <root@osboxes>
2016-11-09 10:15:14 +0000

    arp updates

--- src/counter.c
+++ src/counter.c
@@ -5,4 +5,4 @@
  *      Author: root
  */
 
-
+int request_count;
--- src/main.c
+++ src/main.c
@@ -15,6 +15,10 @@
 
 static uint8_t myip[] = {0xa,0x14,0x28,0xa};
 
+static uint8_t mymac[] = {0xa,0x14,0x28,0xa,0xa,0xa};
+
+extern int request_count;
+
 static void
 usage(const char *progname)
 {
@@ -41,6 +45,7 @@ my_cb(u_char *arg, const struct nm_pkthdr *h, const u_char *buf)
 	struct ether_arp *ahdr;
 
 	(*count)++;
+	request_count++;
 	if (h->flags == 0) {
 		ehdr = (struct ethhdr *)buf;
 		//fprintf(stderr, "received %d bytes at %p count %d slot %p buf_index %d\n",
@@ -51,10 +56,31 @@ my_cb(u_char *arg, const struct nm_pkthdr *h, const u_char *buf)
 
 			if (bytetoint(ahdr->arp_tpa) == bytetoint(myip)) {
 				printf("myip");
-				nm_inject(h->d, buf, h->len);
+				void *addr;
+				struct ethhdr *e2;
+				struct ether_arp *a2;
+
+				addr = (void *)(malloc(sizeof(struct ethhdr) + sizeof(struct ether_arp)));
+				e2 = (struct ethhdr *)addr;
+				memcpy(&e2->h_source, mymac, ETH_ALEN);
+				memcpy(&e2->h_dest, &ehdr->h_source, ETH_ALEN);
+				e2->h_proto = ehdr->h_proto;
+				a2 = (struct ether_arp *)(addr + sizeof(struct ethhdr));
+				memcpy(&a2->ea_hdr, &ahdr->ea_hdr, sizeof(struct arphdr));
+				//a2->arp_hln = ahdr->arp_hln;
+				//a2->arp_hrd = htons(ahdr->arp_hrd);
+				a2->arp_op = htons(ARPOP_REPLY);
+				//a2->arp_pln = ahdr->arp_pln;
+				//a2->arp_pro = htons(ahdr->arp_pro);
+				memcpy(&a2->arp_sha, mymac, ETH_ALEN);
+				memcpy(&a2->arp_spa, myip, 4);
+				memcpy(&a2->arp_tha, &ahdr->arp_sha, ETH_ALEN);
+				memcpy(&a2->arp_tpa, &ahdr->arp_spa, 4);
+
+				nm_inject(h->d, e2, (sizeof(struct ethhdr) + sizeof(struct ether_arp)));
 				return;
 			}
-
+#ifdef DEBUG
 			printf("r:%u, m:%u\t", ahdr->arp_tpa[2], bytetoint(myip));
 			printf("Arp op:%hu", htons(ahdr->ea_hdr.ar_op));
 			printf(" proto:%hu", htons(ahdr->ea_hdr.ar_pro));
@@ -66,6 +92,7 @@ my_cb(u_char *arg, const struct nm_pkthdr *h, const u_char *buf)
 												ehdr->h_source[3],ehdr->h_source[4],ehdr->h_source[5]);
 			printf("d: %x:%x:%x:%x:%x:%x\n", ehdr->h_dest[0],ehdr->h_dest[1],ehdr->h_dest[2],
 														ehdr->h_dest[3],ehdr->h_dest[4],ehdr->h_dest[5]);
+#endif
 		} else if (ehdr->h_proto == htons(ETH_P_IP)) {
 			struct in_addr addr;
 			ihdr = (struct iphdr *)(buf + sizeof(struct ethhdr));
